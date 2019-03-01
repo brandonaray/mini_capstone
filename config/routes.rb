79ee951dd9de5e16ec41623b376b9d5aeadc37e1
brandonaray <brandonray1225@gmail.com>
2019-02-28 18:36:49 -0600

    adds body post request route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     get "/products" => "products#all_products"
     get "/search_product" => "products#one_product"
     get "/search_product/:id" => "products#one_product"
+    post "/search_product_body" => "products#one_product"
   end
 end
