b060d254cf0013b5fd3b6802d6d8c3a3a180eb27
brandonaray <brandonray1225@gmail.com>
2019-03-03 09:56:59 -0600

    cleaned up old code

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -43,19 +43,4 @@ class Api::ProductsController < ApplicationController
     @product.destroy
     render json: {message: "Album successfully deleted"}
   end
-
-  # def gordon
-  #   @gordon = Product.find_by(name: "Gordon")
-  #   render "gordon.json.jbuilder"
-  # end
-
-  # def damn
-  #   @damn = Product.find_by(name: "Damn")
-  #   render "damn.json.jbuilder"
-  # end
-
-  # def nevermind
-  #   @nevermind = Product.find_by(name: "Nevermind")
-  #   render "nevermind.json.jbuilder"
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/products" => "products#index"
-    # get "/search_product" => "products#one_product"
     get "/products/:id" => "products#show"
-    # post "/search_product_body" => "products#one_product"
     post "products" => "products#create"
     patch "products/:id" => "products#update"
     delete "products/:id" => "products#destroy"
