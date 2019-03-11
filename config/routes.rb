ff563c43fa0716213e080ed58aa0b1ea11ae3c09
brandonaray <brandonray1225@gmail.com>
2019-03-11 18:39:52 -0500

    adds user model and user create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
 
   namespace :api do
+    post "/users" => "users#create"
+
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
     post "products" => "products#create"
     patch "products/:id" => "products#update"
     delete "products/:id" => "products#destroy"
-  end
 
-  namespace :api do
     get "/suppliers" => "suppliers#index"
     get "/suppliers/:id" => "suppliers#show"
     post "suppliers" => "suppliers#create"
     patch "suppliers/:id" => "suppliers#update"
     delete "suppliers/:id" => "suppliers#destroy"
-  end
 
-  namespace :api do
     post "images" => "images#create"
     delete "images/:id" => "images#destroy"
   end
