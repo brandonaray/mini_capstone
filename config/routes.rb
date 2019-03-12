347025e5b2b07bd29fd27b2dd873ec82da8e6c21
brandonaray <brandonray1225@gmail.com>
2019-03-11 19:19:30 -0500

    adds json web token authentication (need to correct master key)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     post "/users" => "users#create"
 
+    post "/sessions" => "sessions#create"
+
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
     post "products" => "products#create"
