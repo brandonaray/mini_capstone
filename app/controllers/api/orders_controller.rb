709ab3550500f61ef0c55457eda88378717f67e0
brandonaray <brandonray1225@gmail.com>
2019-03-12 20:40:46 -0500

    add index action for order model and order views

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,25 +1,30 @@
 class Api::OrdersController < ApplicationController
-  tax_rate = 0.0625
 
   def create
-    if current_user
-      @order = Order.new(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      # subtotal: price * quantity,
-      # tax: subtotal * tax_rate,
-      # total: subtotal + tax
-      )
-      if @order.save
-        render json: {message: "Order successful! Your stuff is on its way!"}
-      else
-        render json: {errors: @order.errors.full_messages}, status: :unprocessable_entity
-      end
+    price = Product.find_by(id: params[:product_id]).price
+    quantity = params[:quantity].to_i
+    subtotal = price * quantity
+    tax_rate = 0.0625
+    tax = subtotal * tax_rate
+    total = subtotal + tax
+
+    @order = Order.new(
+    user_id: current_user.id,
+    product_id: params[:product_id],
+    quantity: params[:quantity],
+    subtotal: subtotal,
+    tax: tax,
+    total: total
+    )
+    if @order.save
+      render "show.json.jbuilder"
     else
-      render json: []
+      render json: {errors: @order.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
   def index
+    @orders = current_user.orders
+    render "index.json.jbuilder"
+  end
 end
