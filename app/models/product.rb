d29054d079a0bb5467ad8be1bc7dff180a008945
brandonaray <brandonray1225@gmail.com>
2019-03-14 18:54:45 -0500

    fixes associations between categories and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,7 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
   has_many :orders
-  belongs_to :category_product
+  has_many :category_products
 
   def is_discounted?
     price < 20
