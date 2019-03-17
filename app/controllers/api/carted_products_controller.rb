c232db1581bba1e6e1bfc82b0b9bb3d4c733cb9d
brandonaray <brandonray1225@gmail.com>
2019-03-17 10:25:14 -0500

    add create action for carted products

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,2 +1,12 @@
 class Api::CartedProductsController < ApplicationController
+  def create
+    @carted_product = CartedProduct.new(
+      status: "carted",
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity]
+      )
+    @carted_product.save
+    render json: {message: "Item successfully added to cart"}
+  end
 end
---
+++ app/views/api/carted_products/_carted_products.json.jbuilder
@@ -0,0 +1,5 @@
+json.product carted_product.product.name
+json.quantity carted_product.quantity
+json.subtotal carted_product.subtotal
+json.tax carted_product.tax
+json.total carted_product.total
