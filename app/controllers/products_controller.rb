2e98e6ba18c1e0aa9a46192852783a7f15747a63
brandonaray <brandonray1225@gmail.com>
2019-03-20 19:37:20 -0500

    add HTML delete action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,28 +23,33 @@ class ProductsController < ApplicationController
     redirect_to "/products/#{@product.id}"
   end
 
-  # def edit
-  #   @users = User.all
-  #   @suppliers = Supplier.all
-  #   @product = Product.find_by(id: params[:id])
-  #   render "edit.html.erb"
-  # end
+  def edit
+    @suppliers = Supplier.all
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
 
-  # def update
-  #   @product = Product.find_by(id: params[:id])
-  #   @product.artist = params[:artist]
-  #   @product.year = params[:year]
-  #   @product.label = params[:label]
-  #   @product.price = params[:price]
-  #   @product.description = params[:description]
-  #   @product.name = params[:name]
-  #   @product.supplier_id = params[:supplier_id]
-  #   @product.save
-  #   redirect_to "/products/#{@product.id}"
-  # end
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.artist = params[:artist]
+    @product.year = params[:year]
+    @product.label = params[:label]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.name = params[:name]
+    @product.supplier_id = params[:supplier_id]
+    @product.save
+    redirect_to "/products"
+  end
 
   def show
     @product = Product.find_by(id: params[:id])
     render "show.html.erb"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to "/products"
   end
 end
